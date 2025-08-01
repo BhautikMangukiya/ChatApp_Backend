613f34fde72f348efc2ca64c09399e5defc9158f
BhautikMangukiya <bhautikmangukiya3386@gmail.com>
2025-08-01 16:09:57 +0530

    initial commit

--- server.js
+++ server.js
@@ -54,6 +54,11 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {});
 });
 
+app.get('/', (res, res) => {
+  res.send('server test')
+})
+
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
